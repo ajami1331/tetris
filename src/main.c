8b658fbf585b52fb77c162b17e6bc06d3f971b2e
Araf Al Jami <clown1331@gmail.com>
2023-09-28 12:51:48 +0100

    Draw game area

--- src/main.c
+++ src/main.c
@@ -3,11 +3,39 @@
 #include <emscripten/emscripten.h>
 #endif
 
-int block_size;
+#define HEIGHT_WITH_BORDER          22
+#define WIDTH_WITH_BORDER           12
+#define HEIGHT                      20
+#define WIDTH                       10
 
+int block_size;
+char game_area[HEIGHT][WIDTH] = 
+{
+    "          ",
+    "          ",
+    "          ",
+    "          ",
+    "          ",
+    "*  *      ",
+    "          ",
+    "          ",
+    "          ",
+    "*         ",
+    "          ",
+    "          ",
+    "          ",
+    "          ",
+    "          ",
+    "*  *      ",
+    "          ",
+    "          ",
+    "          ",
+    "*         "
+};
 void tick(void);
 void draw_rectangle_with_border(int x, int y, int width, int height, Color color, Color border_color);
 void draw_game_area_border(void);
+void draw_game_area(void);
 
 int main(void)
 {
@@ -17,7 +45,7 @@ int main(void)
 
     SetTargetFPS(60);
 
-    block_size = GetScreenHeight() / 22;
+    block_size = GetScreenHeight() / HEIGHT_WITH_BORDER;
 
 #if defined(PLATFORM_WEB)
     emscripten_set_main_loop(tick, 0, 1);
@@ -38,29 +66,29 @@ void tick(void)
     BeginDrawing();
 
     ClearBackground(RAYWHITE);
-
-    DrawText("Hello World!", 190, 200, 20, LIGHTGRAY);
     
     draw_game_area_border();
+
+    draw_game_area();
 
     EndDrawing();
 }
 
 void draw_rectangle_with_border(int x, int y, int width, int height, Color color, Color border_color)
 {
-    int border_thickness = (width < height ? width : height) / 10;
+    int border_thickness = (width < height ? width : height) / WIDTH;
     DrawRectangleLinesEx((Rectangle){x, y, width, height}, border_thickness, border_color);
     DrawRectangle(x + border_thickness, y + border_thickness, width - (border_thickness * 2), height - (border_thickness * 2), color);
 }
 
 void draw_game_area_border(void)
 {
-    DrawRectangle(0, 0, 12 * block_size, block_size, RED);
-    DrawRectangle(0, 21 * block_size, 12 * block_size, block_size, RED);
-    DrawRectangle(0, 1 * block_size, block_size, 20 * block_size, RED);
-    DrawRectangle(11 * block_size, 1 * block_size, block_size, 20 * block_size, RED);
-    int border_thickness = block_size / 10;
-    for (int i = 0; i < 22; i++)
+    DrawRectangle(0, 0, WIDTH_WITH_BORDER * block_size, block_size, RED);
+    DrawRectangle(0, 21 * block_size, WIDTH_WITH_BORDER * block_size, block_size, RED);
+    DrawRectangle(0, 1 * block_size, block_size, HEIGHT * block_size, RED);
+    DrawRectangle(11 * block_size, 1 * block_size, block_size, HEIGHT * block_size, RED);
+    int border_thickness = block_size / WIDTH;
+    for (int i = 0; i < HEIGHT_WITH_BORDER; i++)
     {
         DrawRectangleLinesEx((Rectangle){0, i * block_size, block_size, block_size}, border_thickness, BLACK);
         DrawRectangleLinesEx((Rectangle){11 * block_size, i * block_size, block_size, block_size}, border_thickness, BLACK);
@@ -70,5 +98,19 @@ void draw_game_area_border(void)
         
         DrawRectangleLinesEx((Rectangle){i * block_size, 0, block_size, block_size}, border_thickness, BLACK);
         DrawRectangleLinesEx((Rectangle){i * block_size, 21 * block_size, block_size, block_size}, border_thickness, BLACK);
+    }
+}
+
+void draw_game_area(void)
+{
+    for (int i = 0; i < HEIGHT; i++)
+    {
+        for (int j = 0; j < WIDTH; j++)
+        {
+            if (game_area[i][j] == '*')
+            {
+                draw_rectangle_with_border(j * block_size + block_size, i * block_size + block_size, block_size, block_size, RED, BLACK);
+            }
+        }
     }
 }
